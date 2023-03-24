52599cbc58f6417c93532f0f4f0657e6d7fb0bee
ileanaperezn <70903373+ileanaperezn@users.noreply.github.com>
2023-03-24 13:08:51 -0600

    Actualización del Hilo de Santa

---
+++ Santa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package santa;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Santa implements Runnable{
+    private main main;
+
+    public void run() {
+        while(true){
+            esperar(5000);
+            if(main.contador_renos == 9){
+                
+                main.reparteregalos();
+                esperar(5000);
+            }
+             if(main.contador_duendes == 3){
+                 System.out.println("Duendes");
+                 main.ayudaduendes();
+                 esperar(5000);
+             }
+           main.santadormido();
+        }
+        
+    }
+
+    private void esperar(int tiempo) {
+        try {
+            Thread.sleep(tiempo);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Santa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    void setMain(main main) {
+       this.main = main;
+    }
+}
